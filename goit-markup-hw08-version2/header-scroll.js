794d3fa21725352f6492be975275618a795014ca
szmygiel <marta.szmygiel@gmail.com>
2022-12-06 15:15:13 +0100

    header scroll //

--- goit-markup-hw08-version2/header-scroll.js
+++ goit-markup-hw08-version2/header-scroll.js
@@ -1,13 +1,13 @@
-window.onscroll = () => changeHeaderBackground();
+// window.onscroll = () => changeHeaderBackground();
 
-function changeHeaderBackground() {
-  const header = document.querySelector("[data-header]");
-  const headerOffsetTrigger = header.offsetHeight;
-  const pageOffset = window.pageYOffset;
+// function changeHeaderBackground() {
+//   const header = document.querySelector("[data-header]");
+//   const headerOffsetTrigger = header.offsetHeight;
+//   const pageOffset = window.pageYOffset;
 
-  if (pageOffset > headerOffsetTrigger) {
-    header.classList.add("header--no-transparency");
-  } else {
-    header.classList.remove("header--no-transparency");
-  }
-}
+//   if (pageOffset > headerOffsetTrigger) {
+//     header.classList.add("header--no-transparency");
+//   } else {
+//     header.classList.remove("header--no-transparency");
+//   }
+// }
